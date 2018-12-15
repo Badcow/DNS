f48d1d2997f2cd9e140337b972822135cea8916d
Samuel Williams <samuel.williams8@gmail.com>
2018-12-15 17:00:26 +1000

    Test fillOutZone().

--- lib/Tests/ZoneTest.php
+++ lib/Tests/ZoneTest.php
@@ -12,6 +12,10 @@
 namespace Badcow\DNS\Tests;
 
 use Badcow\DNS\Zone;
+use Badcow\DNS\ResourceRecord;
+use Badcow\DNS\Rdata\Factory;
+use Badcow\DNS\AlignedBuilder;
+use Badcow\DNS\ZoneBuilder;
 
 class ZoneTest extends TestCase
 {
@@ -27,5 +31,92 @@ class ZoneTest extends TestCase
 
         //Should throw exception
         $zone->setName('example.com');
+    }
+
+    public function testFillOut()
+    {
+        $zone = new Zone('example.com.');
+        $zone->setDefaultTtl(3600);
+
+        $soa = new ResourceRecord();
+        $soa->setName('@');
+        $soa->setRdata(Factory::Soa(
+            '@',
+            'post',
+            '2014110501',
+            3600,
+            14400,
+            604800,
+            3600
+        ));
+
+        $soa->setClass(\Badcow\DNS\Classes::INTERNET);
+
+        $ns1 = new ResourceRecord();
+        $ns1->setName('@');
+        $ns1->setRdata(Factory::Ns('ns1.nameserver.com.'));
+
+        $ns2 = new ResourceRecord();
+        $ns2->setName('@');
+        $ns2->setRdata(Factory::Ns('ns2.nameserver.com.'));
+
+        $a = new ResourceRecord();
+        $a->setName('sub.domain');
+        $a->setRdata(Factory::A('192.168.1.42'));
+        $a->setComment('This is a local ip.');
+
+        $a6 = new ResourceRecord();
+        $a6->setName('ipv6.domain');
+        $a6->setRdata(Factory::Aaaa('::1'));
+        $a6->setComment('This is an IPv6 domain.');
+
+        $mx1 = new ResourceRecord();
+        $mx1->setName('@');
+        $mx1->setRdata(Factory::Mx(10, 'mail-gw1.example.net.'));
+
+        $mx2 = new ResourceRecord();
+        $mx2->setName('@');
+        $mx2->setRdata(Factory::Mx(20, 'mail-gw2.example.net.'));
+
+        $mx3 = new ResourceRecord();
+        $mx3->setName('@');
+        $mx3->setRdata(Factory::Mx(30, 'mail-gw3.example.net.'));
+
+        $loc = new ResourceRecord();
+        $loc->setName('canberra');
+        $loc->setRdata(Factory::Loc(
+            -35.3075,   //Lat
+            149.1244,   //Lon
+            500,        //Alt
+            20.12,      //Size
+            200.3,      //HP
+            300.1       //VP
+        ));
+        $loc->setComment('This is Canberra');
+
+        $zone->addResourceRecord($loc);
+        $zone->addResourceRecord($mx2);
+        $zone->addResourceRecord($soa);
+        $zone->addResourceRecord($ns1);
+        $zone->addResourceRecord($mx3);
+        $zone->addResourceRecord($a);
+        $zone->addResourceRecord($a6);
+        $zone->addResourceRecord($ns2);
+        $zone->addResourceRecord($mx1);
+
+        $apl = new \Badcow\DNS\Rdata\APL();
+        $apl->addAddressRange(\IPBlock::create('192.168.0.0/23'));
+        $apl->addAddressRange(\IPBlock::create('192.168.1.64/28'), false);
+        $apl->addAddressRange(\IPBlock::create('2001:acad:1::/112'), true);
+        $apl->addAddressRange(\IPBlock::create('2001:acad:1::8/128'), false);
+
+        $multicast = new ResourceRecord('multicast', $apl);
+
+        $zone->addResourceRecord($multicast);
+
+        ZoneBuilder::fillOutZone($zone);
+        $expectation = file_get_contents(__DIR__.'/Resources/example.com_filled-out.txt');
+
+        $this->assertEquals($expectation, AlignedBuilder::build($zone));
     }
 }
