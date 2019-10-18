fd02a2f452a1d5ab48a59545838a35fc29b95e88
Sam Williams <samuel.williams8@gmail.com>
2019-10-18 13:32:40 +1100

    SIG extends RRSIG

--- lib/Rdata/SIG.php
+++ lib/Rdata/SIG.php
@@ -13,43 +13,8 @@ declare(strict_types=1);
 
 namespace Badcow\DNS\Rdata;
 
-// TODO: Implement SIG RData
-class SIG implements RdataInterface
+class SIG extends RRSIG
 {
-    use RdataTrait;
-
     const TYPE = 'SIG';
     const TYPE_CODE = 24;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function toText(): string
-    {
-        // TODO: Implement output() method.
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function toWire(): string
-    {
-        // TODO: Implement toWire() method.
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function fromText(string $text): RdataInterface
-    {
-        // TODO: Implement fromText() method.
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function fromWire(string $rdata): RdataInterface
-    {
-        // TODO: Implement fromWire() method.
-    }
 }
