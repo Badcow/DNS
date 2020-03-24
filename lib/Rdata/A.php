ac88b3d73b6d370dfcbece48121d1bf9e70cd526
samuelwilliams <samuel.williams8@gmail.com>
2020-03-25 09:14:01 +1100

    Suppress error on inet_pton().

--- lib/Rdata/A.php
+++ lib/Rdata/A.php
@@ -65,7 +65,7 @@ class A implements RdataInterface
      */
     public function toWire(): string
     {
-        if (false === $encoded = inet_pton($this->address)) {
+        if (false === $encoded = @inet_pton($this->address)) {
             throw new \InvalidArgumentException(sprintf('The IP address "%s" cannot be encoded. Check that it is a valid IP address.', $this->address));
         }
 
